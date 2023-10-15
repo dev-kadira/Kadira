ea386afa02bad9ccd91e6db8d915395328ff67fa
Kadira <cadira.developer@gmail.com>
2023-10-15 23:38:21 +0200

    chore(Assets) Remove method addModuleToCustomerPortal

--- pkg/vtiger/modules/Assets/modules/Assets/Assets.php
+++ pkg/vtiger/modules/Assets/modules/Assets/Assets.php
@@ -358,8 +358,6 @@ class Assets extends CRMEntity {
 			//Add Assets Module to Customer Portal
 			global $adb;
 
-			$this->addModuleToCustomerPortal();
-
 			include_once('vtlib/Vtiger/Module.php');
 
 			// Mark the module as Standard module
@@ -397,8 +395,6 @@ class Assets extends CRMEntity {
 		} else if($eventType == 'module.preupdate') {
 		// TODO Handle actions before this module is updated.
 		} else if($eventType == 'module.postupdate') {
-			$this->addModuleToCustomerPortal();
-
 			$result = $adb->pquery("SELECT 1 FROM vtiger_modentity_num WHERE semodule = ? AND active =1 ", array($moduleName));
 			if (!($adb->num_rows($result))) {
 				//Initialize module sequence for the module
@@ -406,26 +402,6 @@ class Assets extends CRMEntity {
 			}
 		}
  	}
-
-	function addModuleToCustomerPortal() {
-		$adb = PearDatabase::getInstance();
-
-		$assetsResult = $adb->pquery('SELECT tabid FROM vtiger_tab WHERE name=?', array('Assets'));
-		$assetsTabId = $adb->query_result($assetsResult, 0, 'tabid');
-		if(getTabid('CustomerPortal') && $assetsTabId) {
-			$checkAlreadyExists = $adb->pquery('SELECT 1 FROM vtiger_customerportal_tabs WHERE tabid=?', array($assetsTabId));
-			if($checkAlreadyExists && $adb->num_rows($checkAlreadyExists) < 1) {
-				$maxSequenceQuery = $adb->pquery("SELECT max(sequence) as maxsequence FROM vtiger_customerportal_tabs", array());
-				$maxSequence = $adb->query_result($maxSequenceQuery, 0, 'maxsequence');
-				$nextSequence = $maxSequence+1;
-				$adb->pquery("INSERT INTO vtiger_customerportal_tabs(tabid,visible,sequence) VALUES (?,?,?)", array($assetsTabId,1,$nextSequence));
-			}
-			$checkAlreadyExists = $adb->pquery('SELECT 1 FROM vtiger_customerportal_prefs WHERE tabid=?', array($assetsTabId));
-			if($checkAlreadyExists && $adb->num_rows($checkAlreadyExists) < 1) {
-				$adb->pquery("INSERT INTO vtiger_customerportal_prefs(tabid,prefkey,prefvalue) VALUES (?,?,?)", array($assetsTabId,'showrelatedinfo',1));
-			}
-		}
-	}
 
     /**
 	 * Move the related records of the specified list of id's to the given record.
