be7e910a458c3e533668b1ab7bed54235a489437
Kadira <cadira.developer@gmail.com>
2023-10-21 13:16:42 +0200

    chore(Logger) Turn on log4php.debug only when required

--- include/logging.php
+++ include/logging.php
@@ -1,5 +1,5 @@
 <?php
-/*********************************************************************************
+/*
  * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
  * ("License"); You may not use this file except in compliance with the
  * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
@@ -11,21 +11,28 @@
  * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
  * All Rights Reserved.
  * Contributor(s): ______________________________________.
- ********************************************************************************/
-/*********************************************************************************
+ */
+/*
  * $Header: /advent/projects/wesat/vtiger_crm/sugarcrm/include/logging.php,v 1.1 2004/08/17 13:23:37 gjayakrishnan Exp $
  * Description:  Kicks off log4php.
- ********************************************************************************/
+ */
 
-require_once('config.php');
+require_once 'config.php';
 
 // Performance Optimization: Configure the log folder
-@include_once('config.performance.php');
+@include_once 'config.performance.php';
+
 global $PERFORMANCE_CONFIG;
+
+if (isset($PERFORMANCE_CONFIG, $PERFORMANCE_CONFIG['LOG4PHP_DEBUG']) && $PERFORMANCE_CONFIG['LOG4PHP_DEBUG']) {
+	define('LOG4PHP_DIR', 'libraries/log4php.debug');
+} else {
+	define('LOG4PHP_DIR', 'libraries/log4php');
+}
+// END
 
 define('LOG4PHP_DEFAULT_INIT_OVERRIDE', true);
 define('LOG4PHP_DIR', 'libraries/log4php.debug');
-require_once(LOG4PHP_DIR.'/Logger.php');
-Logger::configure('log4php.properties');
 
-?>
+require_once LOG4PHP_DIR . '/Logger.php';
+Logger::configure('log4php.properties');
